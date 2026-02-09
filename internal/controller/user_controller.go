39b6ca74045331a59229f0fd59d59c22f70f42d5
vvetta <drpsih2004@gmail.com>
2026-02-09 23:28:56 +0300

    base routes

--- internal/controller/user_controller.go
+++ internal/controller/user_controller.go
@@ -1,1 +1,23 @@
 package controller
+
+import (
+	"github.com/WebCraftersGH/User-service/internal/usecase"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type userController struct {
+	svc usecase.UserService
+}
+
+func NewUserController(svc usecase.UserService) *userController {
+	return &userController{svc: svc}
+}
+
+func (c *userController) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/api/v1/users/me", c.GetMe).Methods(http.MethodGet)
+	router.HandleFunc("/api/v1/users/{id}", c.GetUserByID).Methods(http.MethodGet)
+}
+
+func (c *userController) GetMe(w http.ResponseWriter, r *http.Request)       {}
+func (c *userController) GetUserByID(w http.ResponseWriter, r *http.Request) {}
